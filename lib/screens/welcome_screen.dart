f4f78fc9c28e62c745467871f81bd32928a5ec5f
iambrianwalker <bwalker37837@gmail.com>
2026-03-30 18:25:08 -0400

    feat: added welcome and setup to signup screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/signup_screen.dart';
 
 class WelcomeScreen extends StatefulWidget{
 
@@ -6,4 +7,36 @@ class WelcomeScreen extends StatefulWidget{
 
   @override
   State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 20), // space between text and button
+
+            ElevatedButton(
+              onPressed: () {
+                // TODO: navigate to signup screen
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
