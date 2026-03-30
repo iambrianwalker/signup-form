d8479f2aab4745771f84595a53a8e88ae207fc6f
iambrianwalker <bwalker37837@gmail.com>
2026-03-30 18:14:26 -0400

    feat: created welcome screen state

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget{
+
+  const WelcomeScreen(super.key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
